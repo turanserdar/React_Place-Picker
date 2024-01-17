aed3eee43dc5e7499a0d88d14239cc43a12f76b9
Serdar TURAN <turanserdar@outlook.com.tr>
2024-01-16 20:10:42 -0500

    first_commit

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,116 @@
+import { useRef, useState, useEffect, useCallback } from 'react';
+
+import Places from './components/Places.jsx';
+import { AVAILABLE_PLACES } from './data.js';
+import Modal from './components/Modal.jsx';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js'
+
+
+const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storeIds.map(id => AVAILABLE_PLACES.find((place) =>  place.id === id ));
+
+function App() {
+
+
+  const selectedPlace = useRef();
+ const[modalIsOpen, setModalIsOpen]= useState(false);
+ 
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+
+  useEffect(() => {  
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude);
+
+
+      setAvailablePlaces(sortedPlaces);
+
+    });
+
+  }, []); //use Effect doesn't return value it needs two arguments
+  //After this component executed useEffect will be executed
+
+
+
+  function handleStartRemovePlace(id) {
+    setModalIsOpen(true);
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+    //localStorage coming from browser. data will store in browser
+    const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storeIds.indexOf(id) === -1) {
+
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storeIds]));
+
+    }
+
+  }
+
+  const handleRemovePlace= useCallback( function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    setModalIsOpen(false);
+    const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current)));
+  },
+[]
+);
+
+  return (
+    <>
+      <Modal  open={modalIsOpen}>
+      <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={'Select the places you would like to visit below.'}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ 01-starting-project/src/components/Modal.jsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ open, children, onCLose }) {
+  const dialog = useRef();
+
+  useEffect(()=>{
+    if(open)
+    {
+      dialog.current.showModal();
+    }
+    else{
+      dialog.current.close(); 
+    }
+  
+  },[open]);
+
+
+
+
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} onClose={onCLose} >
+      {open ? children:null}
+    </dialog>,
+    document.getElementById('modal')
+  );
+}
+
+export default Modal;
